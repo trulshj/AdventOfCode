56f5c9860adaa7eeb3cc6b93b48fca29abf004f1
Truls Henrik <thj@variant.no>
2022-12-04 11:40:19 +0100

    Create python template

---
+++ 2022/python/dayXX.py
@@ -0,0 +1,24 @@
+from utils import print_day
+
+DAY = 0
+
+
+def main():
+    print_day(DAY, part1, part2, get_data())
+
+
+def get_data():
+    with open(f"../inputs/day{DAY:02}.txt") as f:
+        return []
+
+
+def part1(data):
+    return 0
+
+
+def part2(data):
+    return 0
+
+
+if __name__ == "__main__":
+    main()
