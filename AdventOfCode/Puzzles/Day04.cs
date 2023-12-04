b995e36787aafa058043711cd9ba8763ade54aa2
Truls Henrik Jakobsen <thj@variant.no>
2023-12-04 08:44:41 +0100

    Format linq statements a bit better

--- AdventOfCode/Puzzles/Day04.cs
+++ AdventOfCode/Puzzles/Day04.cs
@@ -10,21 +10,28 @@ public partial class Day04 : BaseDay
     public override string SolvePart1Async()
     {
         Cards = InputFileAsLines
-            .Select(x => CardRegex().Match(x).Groups)
-            .Select(x => new Card(int.Parse(x[1].Value), x[2].Captures.Select(c => int.Parse(c.Value)).ToArray(),
-                x[3].Captures.Select(c => int.Parse(c.Value)).ToArray())).ToList();
+            .Select(line => CardRegex().Match(line).Groups)
+            .Select(group => new Card(
+                int.Parse(group[1].Value),
+                group[2].Captures.Select(capture => int.Parse(capture.Value)).ToArray(),
+                group[3].Captures.Select(capture => int.Parse(capture.Value)).ToArray())).ToList();
 
-        return Cards.Select(x => x.ScratchedNumbers.Count(s => x.WinningNumber.Contains(s))).Where(x => x > 0)
-            .Select(c => Math.Pow(2, c - 1)).Sum().ToString(CultureInfo.InvariantCulture);
+        return Cards
+            .Select(x => x.ScratchedNumbers.Count(s => x.WinningNumber.Contains(s)))
+            .Where(x => x > 0)
+            .Select(c => Math.Pow(2, c - 1))
+            .Sum()
+            .ToString(CultureInfo.InvariantCulture);
     }
 
     public override string SolvePart2Async()
     {
         var copies = Cards.ToDictionary(card => card.CardId, _ => 1);
 
-        var wins = Cards.Select(card =>
-                (cardId: card.CardId, card.ScratchedNumbers.Count(s => card.WinningNumber.Contains(s))))
-            .Where(x => x.Item2 > 0).ToArray();
+        var wins = Cards
+            .Select(card => (cardId: card.CardId, card.ScratchedNumbers.Count(s => card.WinningNumber.Contains(s))))
+            .Where(x => x.Item2 > 0)
+            .ToArray();
 
         foreach (var (cardId, count) in wins)
         {
@@ -32,7 +39,10 @@ public partial class Day04 : BaseDay
             for (var i = 1; i <= count; i++) copies[cardId + i] += cardCount;
         }
 
-        return copies.Select(x => x.Value).Sum().ToString();
+        return copies
+            .Select(x => x.Value)
+            .Sum()
+            .ToString();
     }
 
     [GeneratedRegex(@"Card +(\d+): +(\d+ +)+\|( *\d+)+")]
