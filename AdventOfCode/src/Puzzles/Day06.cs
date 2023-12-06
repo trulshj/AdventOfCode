9e3e0b85ef6ac95e803238188dc3cd065f49fda8
Truls Henrik Jakobsen <thj@variant.no>
2023-12-06 10:11:05 +0100

    Day 06

---
+++ AdventOfCode/src/Puzzles/Day06.cs
@@ -0,0 +1,48 @@
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Puzzles;
+
+public partial class Day06 : BaseDay
+{
+    private long[] Times { get; set; } = Array.Empty<long>();
+    private long[] Distances { get; set; } = Array.Empty<long>();
+
+    private void ParseInput()
+    {
+        var numberLines = InputFileAsLines
+            .Select(line => Numbers().Matches(line)
+                .Select(match => long.Parse(match.Value))
+                .ToArray())
+            .ToArray();
+
+        Times = numberLines[0];
+        Distances = numberLines[1];
+    }
+
+    private static long FindPossibleStarts(long time, long distanceToBeat)
+    {
+        var lowerBound = (long)Math.Floor((time - Math.Sqrt(time * time - 4L * distanceToBeat)) / 2L) + 1L;
+        return time - lowerBound - lowerBound + 1L;
+    }
+
+    public override int SolvePart1()
+    {
+        ParseInput();
+
+        return (int)Times
+            .Zip(Distances)
+            .Select(x => FindPossibleStarts(x.First, x.Second))
+            .Aggregate(1L, (acc, x) => acc * x);
+    }
+
+    public override int SolvePart2()
+    {
+        var time = long.Parse(string.Join("", Times));
+        var distanceToBeat = long.Parse(string.Join("", Distances));
+
+        return (int)FindPossibleStarts(time, distanceToBeat);
+    }
+
+    [GeneratedRegex(@"(\d+)")]
+    private static partial Regex Numbers();
+}
