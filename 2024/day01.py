39fe04b25fc28ebcd44c5bff7a72e4cb24e2054b
Truls Henrik Jakobsen <thj@variant.no>
2024-12-02 15:53:45 +0100

    I love zip

--- 2024/day01.py
+++ 2024/day01.py
@@ -4,19 +4,16 @@ from collections import Counter
 def read_input():
     with open("./input01.txt") as f:
         return [x.rstrip().split() for x in f.readlines()]
-    
-
-x = read_input()
-
-left_list = sorted(int(e[0]) for e in x)
-right_list = sorted(int(e[1]) for e in x)
-
-print(sum(map(lambda x: abs(x[0] - x[1]), zip(left_list, right_list))))
 
 
-occurences = Counter(right_list)
+pairs = read_input()
+
+left, right = map(sorted, zip(*((int(a), int(b)) for a, b in pairs)))
+print(sum(map(lambda x: abs(x[0] - x[1]), zip(left, right))))
+
+occurences = Counter(right)
 similarity = 0
-for n in left_list:
+for n in left:
     similarity += n * occurences[n]
-    
+
 print(similarity)
