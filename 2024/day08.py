164c91e658fd124d2a2a07a1533e93d5547f6a18
Truls Henrik Jakobsen <thj@variant.no>
2024-12-10 14:14:44 +0100

    clean up day 08

--- 2024/day08.py
+++ 2024/day08.py
@@ -2,66 +2,42 @@ from collections import defaultdict
 from itertools import combinations, count
 from operator import add, sub
 
+antennas = defaultdict(list)
 with open("2024/input08.txt") as f:
     grid = f.read().split()
-
-antennas = defaultdict(list)
-
-for y in range(len(grid)):
-    for x in range(len(grid[0])):
-        if grid[y][x] != '.':
-            antennas[grid[y][x]].append((y, x))
-
-antenodes = set()
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell != '.':
+                antennas[cell].append((y, x))
 
 
-def calc_antenodes(y1: int, x1: int, y2: int, x2: int, n=1):
-    dy = y2 - y1
-    dx = x2 - x1
-
-    ans = set()
-
-    for (y, x) in [(y1, x1), (y2, x2)]:
+def calculate_antenodes(a: tuple[int, int], b: tuple[int, int], n: int):
+    dy, dx = b[0] - a[0], b[1] - a[1]
+    for (y, x) in [a, b]:
         for op in [add, sub]:
-            ans.add((op(y, dy*n), op(x, dx*n)))
-
-    return list(ans)
+            new_point = (op(y, dy * n), op(x, dx * n))
+            if n == 0 or (new_point != a and new_point != b):
+                yield new_point
 
 
 def in_bounds(y: int, x: int):
-    return (0 <= y < len(grid) and 0 <= x < len(grid[0]))
+    return 0 <= y < len(grid) and 0 <= x < len(grid[0])
 
 
-for group in antennas.values():
-    for a, b in combinations(group, 2):
-        for an in calc_antenodes(*a, *b):
-            if in_bounds(*an) and an != a and an != b:
-                antenodes.add(an)
-
-
-def get_antenodes():
+def calculate_harmonics(only_first=False):
     antenodes = set()
     for group in antennas.values():
         for a, b in combinations(group, 2):
-            for i in count(1):
-                ans = calc_antenodes(*a, *b, i)
-                valid_nodes = list(
-                    filter(lambda x: x[1], zip(ans, map(lambda x: in_bounds(*x), ans))))
-                if len(valid_nodes) == 0:
+            harmonic_range = [1] if only_first else count()
+            for n in harmonic_range:
+                valid_nodes = {node for node in
+                               calculate_antenodes(a, b, n) if in_bounds(*node)}
+
+                if not valid_nodes:
                     break
-                for n in valid_nodes:
-                    antenodes.add(n)
+                antenodes.update(valid_nodes)
+    return len(antenodes)
 
 
-def debug_antenodes():
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            if (y, x) in antenodes:
-                print('#', end='')
-            else:
-                print(grid[y][x], end='')
-        print()
-
-
-print(len(antenodes))
-print(len(antenodes2))
+print("Part 1:", calculate_harmonics(only_first=True))
+print("Part 2:", calculate_harmonics())
