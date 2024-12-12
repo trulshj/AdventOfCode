271d8b9ae1072405b4727e0f34accf70526b7735
Truls Henrik Jakobsen <thj@variant.no>
2024-12-12 13:22:20 +0100

    List comprehension in day 12

--- 2024/day12.py
+++ 2024/day12.py
@@ -11,9 +11,9 @@ def get_grid(y, x):
 
 ORTHO = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 DIAGONAL = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
 visited = set()
 garden_plots = []
-
 
 for y, row in enumerate(grid):
     for x, cell in enumerate(row):
@@ -26,6 +26,7 @@ for y, row in enumerate(grid):
             c = stack.pop()
             if c in visited:
                 continue
+
             plot.add(c)
             visited.add(c)
 
@@ -39,26 +40,23 @@ for y, row in enumerate(grid):
 def calculate_cost(garden_plot: set[tuple[int, int]]):
     area = len(garden_plot)
 
-    perimeter = 0
-    for y, x in garden_plot:
-        p = 4
-        for dy, dx in ORTHO:
-            p -= (y+dy, x+dx) in garden_plot
-        perimeter += p
+    perimeter = sum(
+        4 - sum((y + dy, x + dx) in garden_plot for dy, dx in ORTHO)
+        for y, x in garden_plot
+    )
 
-    sides = 0
-    for y, x in garden_plot:
-        for dy, dx in DIAGONAL:
-            # Vertical, Horizontal, Diagonal
-            v, h, d = (y+dy, x), (y, x+dx), (y+dy, x+dx)
-            sides += v not in garden_plot and h not in garden_plot  # Outer Corner
-            sides += v in garden_plot and h in garden_plot and d not in garden_plot  # Inner Corner
+    sides = sum(
+        (v not in garden_plot and h not in garden_plot) +  # Outer Corner
+        (v in garden_plot and h in garden_plot and d not in garden_plot)  # Inner Corner
+        for y, x in garden_plot
+        for dy, dx in DIAGONAL
+        for v, h, d in [((y + dy, x), (y, x + dx), (y + dy, x + dx))]
+    )
 
     return area, perimeter, sides
 
 
-p1 = 0
-p2 = 0
+p1 = p2 = 0
 for plot in garden_plots:
     area, perimeter, sides = calculate_cost(plot)
     p1 += area * perimeter
