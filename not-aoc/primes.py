f07f0eb7f1cbc79fe8187b3e1118a85ddf912b5f
Truls Henrik Jakobsen <thj@variant.no>
2025-12-02 10:36:18 +0100

    prime stuff

--- not-aoc/primes.py
+++ not-aoc/primes.py
@@ -50,16 +50,8 @@ def fast_primes(n):
 
 
 SIEVE_SIZE = 1_000
-# primes = [i for i in range(2, 10_000_000) if is_prime(i)]
 primes = fast_primes(SIEVE_SIZE)
 sieve = [True] * SIEVE_SIZE
-
-# for prime in primes:
-#     i = prime - 1
-#     while i < len(sieve)-1:
-#         sieve[i] = False
-#         i += prime
-#     print(prime, sum(sieve) / len(sieve))
 
 
 @cache
@@ -82,28 +74,30 @@ class Fraction:
     def __init__(self, numerator: int, denominator: int):
         self.numerator = numerator
         self.denominator = denominator
-        
+
     def __repr__(self):
         return f"{self.numerator}/{self.denominator}"
-    
+
     def __mul__(self, other):
         numerator = self.numerator * other.numerator
         denominator = self.denominator * other.denominator
         divisor = gcd(numerator, denominator)
         return Fraction(numerator // divisor, denominator // divisor)
-    
+
     def __add__(self, other):
-        numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        numerator = self.numerator * other.denominator + \
+            other.numerator * self.denominator
         denominator = self.denominator * other.denominator
         divisor = gcd(numerator, denominator)
         return Fraction(numerator // divisor, denominator // divisor)
-    
+
     def __sub__(self, other):
-        numerator = self.numerator * other.denominator - other.numerator * self.denominator
+        numerator = self.numerator * other.denominator - \
+            other.numerator * self.denominator
         denominator = self.denominator * other.denominator
         divisor = gcd(numerator, denominator)
         return Fraction(numerator // divisor, denominator // divisor)
-    
+
     def decimal(self):
         return self.numerator / self.denominator
 
@@ -113,11 +107,7 @@ def calculate_fraction(primes: list[int]):
     for i in range(len(primes)):
         result *= Fraction(primes[i] - 1, primes[i])
         yield result
-    
 
-
-# for i in range(1, len(primes)+1):
-#     print(f"n: {i}, p_n: {primes[i-1]}, remainder: {f(i)}")
 
 for i, fraction in enumerate(calculate_fraction(primes), 1):
     print(f"n: {i}, fraction: {fraction}, decimal: {fraction.decimal()}")
